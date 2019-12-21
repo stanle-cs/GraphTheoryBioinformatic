30b8956b83fc90279699a2ad3da348fb934d793b
Stan Le <stanle.work@gmail.com>
2019-12-21 15:09:25 -0600

    General rewritten of the string_construction.cpp file. Streamline the functions.

--- stringfromkmer/string_reconstruction.cpp
+++ stringfromkmer/string_reconstruction.cpp
@@ -3,21 +3,12 @@
 #include <vector>
 #include <map>
 #include <set>
-#include <unordered_set>
+#include <chrono>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::vector;
-using std::pair;
-using std::map;
-using std::unordered_set;
-using std::set;
-using std::multiset;
+using namespace std;
+using namespace std::chrono;
 
-
-struct graph {
+typedef struct graph {
     multiset<pair<string, string>> edges;
     set<string> nodelist;
     map<string, int> outlet;
@@ -38,7 +29,25 @@ struct graph {
         outlet[origin]--;
         inlet[destination]--;
     }
-};
+    string toString() {
+        string output = "";
+        output += "nodelist:\n";
+        for (auto const& node: nodelist) {
+            output += node + "\n";
+        }
+        output += "edges:\n";
+        for (auto const& edge: edges) {
+            output += edge.first + "->" + edge.second + "\n";
+        }
+        output += "deBruijin:\n";
+        for (auto const& node: deBruijin) {
+            for (auto const& dest: node.second) {
+                output += node.first + "->" + dest + "\n";
+            }
+        }
+        return output;
+    }
+} graph;
 
 /** Process the input from graph into printable string
  * 
@@ -55,21 +64,6 @@ string output_process(vector<pair<string, string>> eulerian_path) {
     }
 
     return output;
-}
-
-/** Process the input from input into machine readable struct graph
- * 
- * \param graph_input   a vector of string obtained from the input containing
- *                      entries in the form of "X -> Y,Z"
- * 
- * \return  a struct graph that contain edge list, node list, size, and inlet,
- *          outlet count
- */
-struct graph input_process(vector<string> graph_input) {
-    struct graph eulerian_path;
-
-    
-    return eulerian_path;  
 }
 
 /** Retravel a Eulerian path and get to the designated position start
@@ -97,6 +91,7 @@ vector<pair<string, string>> retravelpath(vector<pair<string,string>> path, stri
  * \param &source   the source to be returned to
  */
 void findsinksource(struct graph graph, string &sink, string &source) {
+    //cout << graph.toString();
     for (auto const& node: graph.nodelist) {
         if (graph.inlet[node] < graph.outlet[node]) {
             source = node;
@@ -105,9 +100,8 @@ void findsinksource(struct graph graph, string &sink, string &source) {
             sink = node;
         }
     }
-    cout << "source: " << source << " sink: " << sink << "\n";
+    //cout << "source: " << source << " sink: " << sink << "\n";
 }
-
 
 /** construct a Eulerian Cycle using the input graph
  * \param eulerian_graph    a graph that contain a cycle
@@ -117,7 +111,7 @@ void findsinksource(struct graph graph, string &sink, string &source) {
 vector<pair<string, string>> eulerian_cycle(struct graph eulerian_graph, string start) {
 
     vector<pair<string, string>> eulerian_path;
-    unordered_set<string> cyclenodes;
+    set<string> cyclenodes;
     int total_egde = eulerian_graph.edges.size();
 
     //while there are still unaccounted edge
@@ -157,87 +151,19 @@ vector<pair<string, string>> eulerian_cycle(struct graph eulerian_graph, string 
     return eulerian_path;
 }
 
-
-/** Finds an Eulerian path in the given graph
- *
- * \param graph     a vector of strings containing edges in the graph;
- *                  one string may correspond to multiple edges if it
- *                  is of the form "X -> Y,Z"
- *
- * \return a string containing an Eulerian path through the given graph
+/**
+ * 
+ * 
+ * 
  */
-string eulerian_path(const vector<string>& graph) {
-    struct graph eulerian_graph;
+string eulerian_path(graph eulerian_graph) {
+    // the eulerian path in the vector of string pairs form
     vector<pair<string, string>> eulerian_path;
+    // the sink and source where we start and finish
     string sink, source;
+    // the placeholder for output
     string output = "";
 
-    /*input processing
-    --------------------------------------------------
-    */
-    for (int i =0; i < graph.size(); i++) {
-        //define temporary stuff to be used during processing
-        string temp = graph[i];
-        string origin = "";
-        string destination = "";
-        //catch the origin node
-        int origin_end = temp.find(" -> ",0);
-        origin = temp.substr(0, origin_end);
-        //check if origin is a registered node, if not initialize it
-        if (eulerian_graph.outlet.count(origin) != 1) {
-            eulerian_graph.outlet[origin] = 0;
-        }
-        //delete it from the temp string
-        temp.erase(0, origin_end + 4);
-        //add "," for uniformity
-        temp += ",";
-        //while there are ","s remained in the temp string
-        while (temp.find(",", 0) != -1) {
-            //catch this destination node
-            int dest_end = temp.find(",", 0);
-            destination = temp.substr(0, dest_end);
-            //append origin to the nodelist
-            if (eulerian_graph.nodelist.count(origin) != 1) {
-                eulerian_graph.nodelist.insert(origin);
-            }
-            //append destination to the nodelist
-            if (eulerian_graph.nodelist.count(destination) != 1) {
-                eulerian_graph.nodelist.insert(destination);
-            }
-            //finalize the edge and store it into the edge list
-            eulerian_graph.edges.insert({origin, destination});
-            //add the edge to the deBruijin Graph
-            eulerian_graph.deBruijin[origin].insert(destination);
-            //increase the outlet count of origin
-            eulerian_graph.outlet[origin]++;
-            //check if destination is a registered node, if not then initialize it
-            if (eulerian_graph.inlet.count(destination) == 0) {
-                eulerian_graph.inlet[destination] = 1;
-            }
-            //else we increase its inlet by one
-            else {
-                eulerian_graph.inlet[destination]++;
-            }
-            //delete that part
-            temp.erase(0, dest_end + 1);
-        }
-    }
-    //fill in the zeros in inlet list
-    for (auto const& node: eulerian_graph.nodelist) {
-        if (eulerian_graph.inlet.count(node) == 0) {
-            eulerian_graph.inlet[node] = 0;
-        }
-    }
-    //fill in the zeros in outlet list
-    for (auto const& node: eulerian_graph.nodelist) {
-        if (eulerian_graph.outlet.count(node) == 0) {
-            eulerian_graph.outlet[node] = 0;
-        }
-    }
-    //define the size of graph
-    eulerian_graph.N = eulerian_graph.nodelist.size();
-
-    
     //find sink and source
     sink = "";
     source = "";
@@ -248,6 +174,7 @@ string eulerian_path(const vector<string>& graph) {
         set<string>::iterator cycle_it = eulerian_graph.nodelist.begin();
         //cout << "\nNode chosen as start: " << *cycle_it << "\n";
         eulerian_path = eulerian_cycle(eulerian_graph, *cycle_it);
+        //eulerian_path.pop_back();
         output = output_process(eulerian_path);
         return output;
     }
@@ -269,7 +196,6 @@ string eulerian_path(const vector<string>& graph) {
     return output;
 }
 
-
 /**
  * Creates a de Bruijn graph from a colletion of k-mers
  *
@@ -279,9 +205,10 @@ string eulerian_path(const vector<string>& graph) {
  * \return a string containing an adjacency list representation of the
  * de Bruijn graph as described in the problem specification
  */
-vector<string> de_bruijn(const vector<string>& patterns) {
+graph inputProcess(const vector<string>& patterns) {
     vector<string> output;
     map<string, vector<string>> deBruijinGraph;
+    graph eulerian_graph;
 
     //construct map
     //for each line of input
@@ -292,19 +219,61 @@ vector<string> de_bruijn(const vector<string>& patterns) {
         deBruijinGraph[edge].push_back(patterns[i].substr(1, patterns[i].length() - 1));
     }
 
-    //output map
-    for (auto const& edge: deBruijinGraph) {
-	string line;
-    line += edge.first + " -> ";
-	for (int i = 0; i < edge.second.size(); i++) {
-	    line += edge.second[i] + ",";
-	}
-	line.erase(line.length() -1);
-    output.push_back(line);
-    cout << line << "\n";
+
+    // loop through the deBruijin graph
+    for (auto const& node: deBruijinGraph) {
+        for (int i = 0; i < node.second.size(); i++) {
+            //do actual work here
+            string origin = node.first, destination = node.second[i];
+
+            // append origin to the nodelist
+            if (eulerian_graph.nodelist.count(origin) != 1) {
+                eulerian_graph.nodelist.insert(origin);
+            }
+
+            // append destination to the nodelist
+            if (eulerian_graph.nodelist.count(destination) != 1) {
+                eulerian_graph.nodelist.insert(destination);
+            }
+            // finalize the edge and store it into the edge list
+            eulerian_graph.edges.insert({origin, destination});
+            // add the edge to the deBruijin Graph
+            eulerian_graph.deBruijin[origin].insert(destination);
+            // increase the outlet count of origin
+            eulerian_graph.outlet[origin]++;
+            // check if destination is a registered node, if not then initialize it
+            if (eulerian_graph.inlet.count(destination) == 0) {
+                eulerian_graph.inlet[destination] = 1;
+            }
+            // else we increase its inlet by one
+            else {
+                eulerian_graph.inlet[destination]++;
+            }
+
+            /*------CHECKS - TO BE DELETED------*/
+            //cout << node.first << "->" << node.second[i] << "\n";
+        }
     }
-    return output;
+    
+    // fill in the zeros in inlet list
+    for (auto const& node: eulerian_graph.nodelist) {
+        if (eulerian_graph.inlet.count(node) == 0) {
+            eulerian_graph.inlet[node] = 0;
+        }
+    }
+    // fill in the zeros in outlet list
+    for (auto const& node: eulerian_graph.nodelist) {
+        if (eulerian_graph.outlet.count(node) == 0) {
+            eulerian_graph.outlet[node] = 0;
+        }
+    }
+    // define the size of graph
+    eulerian_graph.N = eulerian_graph.nodelist.size();
+
+
+    return eulerian_graph;
 }
+
 
 /*
  * Reconstructs a string from its k-mer composition
@@ -314,12 +283,15 @@ vector<string> de_bruijn(const vector<string>& patterns) {
  *
  * \return a string text with k-mer composition equal to patterns
  */
+
+
 string reconstruct(int k, const vector<string>& patterns) {
-    string output;
-    output = eulerian_path(de_bruijn(patterns));
+    // TODO: your code here
+    string output = "";
+    output = eulerian_path(inputProcess(patterns));
+
     return output;
 }
-
 
 int main(int argc, char* argv[]) {
     int k;
@@ -332,5 +304,10 @@ int main(int argc, char* argv[]) {
         patterns.push_back(pattern);
     }
 
+    auto start = high_resolution_clock::now(); 
     cout << reconstruct(k, patterns) << endl;
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<seconds>(stop - start); 
+    if (duration.count() > 0) cout << duration.count() << endl;
+
 }
